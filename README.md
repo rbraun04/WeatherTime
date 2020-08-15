1c2c39f244738d9751265ad215b3df897ccad190
Ryan Braun <ryan@thelyricla.com>
2020-08-14 18:11:39 -0700

    updated the button functions so that you can grab from local storage and search the previous searches

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # WeatherTime
+
+August 18, 2020
+
+I started this homework by watching video that walks you through the AJAX function with Open Weather.  I was able to get the AJAX function to retrieve the inforamtion with my key and push it to the screen.  I have figured out how to get the 5 day forecast by indentifying the date and location within the array that is sent back by Open Weather.  I will need to format the responses into different days and identify all the elemnts that are needed.
+
+I am using the on click function to grab the input search and list it on the screen.  I am not understanding how to recall the search historhy if I reload the page, the items are erased.  
+
+
+August 19, 2020
+
+I struggled a lot with finding the ID for the function pull but managed to get it working.
--- script.js
+++ script.js
@@ -5,13 +5,20 @@
     var savedSearch = [];
 
     // get item from storage and append it to the screen
-     var savedSearch = JSON.parse(localStorage.getItem("User"))
-        if(savedSearch !== null) {
-            for (var i = 0; i<savedSearch.length; i++) {
-                $(".searchhistory").html("<button type='button' class='btn btn-primary'>" + savedSearch[i] + "</button>")
+     var searchElem = JSON.parse(localStorage.getItem("User"))
+        if(searchElem !== null) {
+            for (var i = 0; i<searchElem.length; i++) {
+                $(".searchhistory").append("<div><a class='saved'> " + searchElem[i] +  "</a></div>");
+               
+                var a = $(".saved");
+                a.attr("id", searchElem[i]);
+                
         }};
 
-    var searchElem = [];
+
+
+    
+var searchElem = [];
    
    
    
@@ -22,12 +29,16 @@ $('#submit').click (function() {
 
         searchElem.push(cityElem.value)
         localStorage.setItem("User", JSON.stringify(searchElem));
-        $(".searchhistory").html("<button type='button' class='btn btn-primary'>" + searchElem + "</button>")
+        $(".searchhistory").append("<div><a class='saved'> "  + cityElem.value + "</a></div>");
+        
+        var a = $(".saved");
+        a.attr("data-name", cityElem.value);
         
         console.log(searchElem)
 
     // Request the API for current weather conditions
         var city = $("#city").val();
+        console.log(city)
         
             if (city !== '') {
                     $.ajax ({
@@ -84,3 +95,53 @@ function show2 (data1) {
            
     }
  
+// run this function whe user clicks saved city
+
+$('.saved').click (function() {
+    // Save the City Name to Local Storage
+
+// Request the API for current weather conditions
+        var city = $(".saved").map(function() {
+            return this.id
+        }).get();
+        console.log(city)
+    
+        if (city !== '') {
+                $.ajax ({
+                    url: 'http://api.openweathermap.org/data/2.5/weather?q=' + city + "&units=metric" + "&APPID=2e7712007ae88c6a7b26e1e008ca4659",
+                    type: "GET",
+                    dataType:"jsonp",
+                    success : function (data) {
+                        console.log (data);
+                        var widget = show(data);
+
+                        $("#show").html(widget);
+
+                        $("city").val('');
+                    }
+                                   
+               
+                })
+                $.ajax ({
+                    url: 'http://api.openweathermap.org/data/2.5/forecast?q=' + city + "&units=metric" + "&APPID=2e7712007ae88c6a7b26e1e008ca4659",
+                    type: "GET",
+                    dataType:"jsonp",
+                    success : function (data1) {
+                        console.log (data1);
+                        var widget1 = show2(data1);
+
+                        $("#show2").html(widget1);
+
+                        $("5daycity").val('');
+                    }
+                                   
+               
+                });
+            }else{
+                $("#error").html('Field cannot be empty');
+
+                
+            }
+});
+
+    
