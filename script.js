5bfd3d9134ec99d89addeccb055b5a1dab5c0871
Ryan Braun <ryan@thelyricla.com>
2020-08-16 09:32:34 -0700

    fixed run previous search function to display forecast when opening page

--- script.js
+++ script.js
@@ -12,6 +12,90 @@
                
                 var a = $(".saved");
                 a.attr("id", searchElem[i]);
+                $.ajax ({
+                    url: 'http://api.openweathermap.org/data/2.5/weather?q=' + searchElem[0] + "&units=metric" + "&APPID=2e7712007ae88c6a7b26e1e008ca4659",
+                    type: "GET",
+                    dataType:"jsonp",
+                }).then(function (data) {
+                        console.log (data);
+                        var widget = show(data),
+
+                        //store coordinates into locationElem to use for UVI Ajax function
+                        latitude= data.coord.lat
+                        longitude = data.coord.lon
+
+                        console.log(latitude)
+                        console.log(longitude)
+                        $("#show").html(widget);
+                    }).then(function(data3) {
+                        console.log(latitude)
+                        
+                    // ajax call for UVI index   
+                    $.ajax ({
+                        url: 'http://api.openweathermap.org/data/2.5/uvi?appid=2e7712007ae88c6a7b26e1e008ca4659' + '&lon=' + longitude + '&lat=' + latitude,
+                        
+                        type: "GET",
+                        dataType:"json",
+                        success : function (data2) {
+                        console.log (data2);
+                            
+                        var widget1 = show2(data2);
+
+                        $("#show3").html(widget1);
+
+                        $("uvi").val('');
+                        }
+                    })});
+
+                    //ajax call for 5 day forecast
+                    $.ajax ({
+                        url: 'http://api.openweathermap.org/data/2.5/forecast?q=' + searchElem[0] + "&units=metric" + "&APPID=2e7712007ae88c6a7b26e1e008ca4659",
+                        type: "GET",
+                        dataType:"jsonp",
+                        success : function (data1) {
+                            console.log (data1);
+                            var widget1 = show2(data1);
+
+                            $("#show2").html(widget1);
+
+                            $("5daycity").val('');
+                        }
+                    })
+                    
+                    function show (data) {
+                        latitude= data.coord.lat
+                        longitude = data.coord.lon
+                        var iconcode = data.weather[0].icon;
+                        var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
+                        $('#wicon').attr('src', iconurl);
+                    
+                        return "<h3><strong>Weather</strong>: " + data.weather[0].main +"<h3>" +
+                        "<h3><strong>Location</strong>: " + data.name +"<h3>" +
+                        "<h3><strong>Date</strong>: " + data.weather[0].main +"<h3>" +
+                        "<h3><strong>Temperature</strong>: " + data.weather[0].main +"<h3>" +
+                        "<h3><strong>Humidity</strong>: " + data.weather[0].main +"<h3>" +
+                        "<h3><strong>Wind Speed</strong>: " + data.weather[0].description +"<h3>" +
+                        "<h3><strong>UV Index</strong>: " + data.weather[0].main +"<h3>";
+                        }
+                    
+                                   
+                                
+                    function show2 (data1) {           
+                        return "<h3><strong>Weather</strong>: " + data1.list[0].weather[0].main +"<h3>" +
+                                "<h3><strong>Description</strong>: " + data1.list[0].description +"<h3>" +
+                                "<h3><strong>Temperature</strong>: " + data1.list[0].main +"<h3>";
+                                console.log(data1)
+                               
+                        }
+                        
+                            
+                        
+           
+    
+                        
+
+
+
                 
         }};
 
@@ -115,8 +199,11 @@ latitude1 = latitude
 function show (data) {
     latitude= data.coord.lat
     longitude = data.coord.lon
+    var iconcode = data.weather[0].icon;
+    var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
+    $('#wicon').attr('src', iconurl);
+
     return "<h3><strong>Weather</strong>: " + data.weather[0].main +"<h3>" +
-    "<h3><strong>icon</strong>: " + data.weather[0].icon + ".png" +"<h3>" +
     "<h3><strong>Location</strong>: " + data.name +"<h3>" +
     "<h3><strong>Date</strong>: " + data.weather[0].main +"<h3>" +
     "<h3><strong>Temperature</strong>: " + data.weather[0].main +"<h3>" +
@@ -206,4 +293,4 @@ $('.saved').click (function() {
             
         }
 });
-});
+})
